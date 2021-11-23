583c6c92e05a4582ad9acbbeb132f9f2fb9474d4
Jonathan Cardenas <jonscardenas@gmail.com>
2021-11-23 11:17:49 -0600

    made sorter into abstract class

--- build/Sorter.js
+++ build/Sorter.js
@@ -2,17 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Sorter = void 0;
 class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    //when adding string, remember that union (pipe) operator only allows for objects that share a common property
     sort() {
-        //const length = this.collection.length;
-        const { length } = this.collection;
+        const { length } = this;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection.compare(j, j + 1)) {
-                    this.collection.swap(j, j + 1);
+                if (this.compare(j, j + 1)) {
+                    this.swap(j, j + 1);
                 }
             }
         }
